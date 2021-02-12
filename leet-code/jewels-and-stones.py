b9a54c07c62198091d15476f55ab06f3a9322e5a
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-12 19:37:01 -0300

    771. Jewels and Stones

---
+++ leet-code/jewels-and-stones.py
@@ -0,0 +1,25 @@
+"""
+771. Jewels and Stones
+
+You're given strings jewels representing the types of stones that are jewels, and stones
+representing the stones you have. Each character in stones is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+"""
+
+class Solution:
+    def numJewelsInStones(self, jewels, stones):
+        total = 0
+
+        for jewel in jewels:
+            total += stones.count(jewel)
+
+        return total
+
+
+
+# Tests
+
+print(Solution().numJewelsInStones("aA", "aAAbbbb"))  # 3
+print(Solution().numJewelsInStones("z", "ZZ"))  # 0
